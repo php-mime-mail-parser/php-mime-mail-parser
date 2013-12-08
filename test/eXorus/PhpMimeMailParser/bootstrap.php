ba5851e765166b8a2126d2e7f967bb96ab1a04db
eXorus <exorus.spam@gmail.com>
2013-12-08 16:13:37 +0100

    Add bootstrap and phpunit.xml

---
+++ test/eXorus/PhpMimeMailParser/bootstrap.php
@@ -0,0 +1,6 @@
+<?php
+
+define('DS', DIRECTORY_SEPARATOR);
+define('APP_ROOT', dirname(__DIR__) . DS);
+define('APP_TEST', __DIR__ . DS);
+define('APP_SRC', 'src' . DS . 'eXorus' . DS . 'PhpMimeMailParser' . DS);
