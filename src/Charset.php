66f41b7ed4f0660f6d2e290d7971edaf4a83b18f
KentaKatoh <kenta@diverta.co.jp>
2015-04-28 16:57:41 +0900

    change iconv to mb_convert_encoding.

--- src/Charset.php
+++ src/Charset.php
@@ -323,7 +323,7 @@ class Charset implements CharsetManager
         if (strtolower($charset) == 'utf-8' || strtolower($charset) == 'us-ascii') {
             return $encodedString;
         } else {
-            return iconv($this->getCharsetAlias($charset), 'UTF-8//TRANSLIT', $encodedString);
+            return mb_convert_encoding($encodedString, 'UTF-8',$this->getCharsetAlias($charset));
         }
     }
 
