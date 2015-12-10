ed119631925e320a838a51c673d161b31a9ae801
Mathias Chouet <mathias@tela-botanica.org>
2015-12-10 13:44:26 +0100

    zero-length body fix : back to original code added a test with a zero-length body example message

--- src/Parser.php
+++ src/Parser.php
@@ -510,11 +510,12 @@ class Parser
     {
         $start = $part['starting-pos-body'];
         $end = $part['ending-pos-body'];
-        if ($end-$start <= 0) {
-            return '';
+		$body = '';
+        if ($end-$start > 0) {
+            fseek($this->stream, $start, SEEK_SET);
+            $body = fread($this->stream, $end-$start);
         }
-        fseek($this->stream, $start, SEEK_SET);
-        return fread($this->stream, $end-$start);
+        return $body;
     }
 
     /**
