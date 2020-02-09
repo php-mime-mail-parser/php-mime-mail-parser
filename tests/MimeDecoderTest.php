677f966e04b581e88fcdee4c3627805f4c1b0284
Alexey Kopytko <alexey@kopytko.com>
2020-02-10 00:24:14 +0900

    Since we depend on iconv already, let's use iconv_mime_decode

--- tests/MimeDecoderTest.php
+++ tests/MimeDecoderTest.php
@@ -16,6 +16,7 @@ final class MimeDecoderTest extends TestCase
  =?windows-1250?Q?uki_ci=B9gle_na_Ciebie_czeka?=',
             'Automatyczna odpowiedź: Piotrze, test z 6 miesięcy nauki ciągle na Ciebie czeka'],
         ['=?iso-2022-jp?B?GyRCJygnJS1iGyhCNDEgGyRCJ2AnZBsoQiAyOC4wOS4yMDE2?=', 'ЖД№41 от 28.09.2016'],
+        ['=?iso-2022-jp?Q?=1B$B-!-"=1B(B?=', '①②'],
         ['=?iso-8859-8-i?B?7vLw5CAn4PDpIOzgIPDu9uAnOiBJbnZvaWNlIDAyNzIyMDI3?=', "מענה 'אני לא נמצא': Invoice 02722027"],
     ];
 
