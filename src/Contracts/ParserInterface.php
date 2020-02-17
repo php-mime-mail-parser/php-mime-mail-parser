983a3083b7c3c51b93165245387ea4f23f14b1a7
Vincent Dauce <eXorus@users.noreply.github.com>
2020-02-17 13:48:56 +0100

    Update ParserInterface.php

--- src/Contracts/ParserInterface.php
+++ src/Contracts/ParserInterface.php
@@ -7,7 +7,29 @@ use PhpMimeMailParser\Attachment;
 
 interface ParserInterface
 {
+    /**
+     * Attachment filename argument option for ->saveAttachments().
+     */
+    const ATTACHMENT_DUPLICATE_THROW  = 'DuplicateThrow';
+    const ATTACHMENT_DUPLICATE_SUFFIX = 'DuplicateSuffix';
+    const ATTACHMENT_RANDOM_FILENAME  = 'RandomFilename';
 
+    /**
+     * Attachment types to include for ->getAttachment()
+     */
+    const GA_INCLUDE_INLINE = 1; // include inline and top-level attachments
+    const GA_INCLUDE_NESTED = 2; // all non-inline attachments, including nested
+    const GA_INCLUDE_ALL = 3;    // inline and nested attachments
+    const GA_TOPLEVEL = 0;       // only non-inline top-level attachments
+    
+
+    /**
+     * Set the file path we use to get the email text
+     *
+     * @param string $path File path to the MIME mail
+     *
+     * @return Parser MimeMailParser Instance
+     */
     public function setPath(string $path): ParserInterface;
 
     /**
